fcc11b229159becddd51addeeecfe9025171d139
Vlad Iliescu <vladiliescu@users.noreply.github.com>
2025-01-07 17:41:08 +0200

    fix: process page with readabilipy if readability.js fails

--- grabit.py
+++ grabit.py
@@ -278,6 +278,14 @@ def extract_readable_content_and_title(html_content, use_readability_js):
             html_content, use_readability=use_readability_js
         )
         content_html = rpy.get("content", "")
+
+        # If readability.js fails, try again without it
+        if not content_html and use_readability_js:
+            rpy = simple_json_from_html_string(html_content, use_readability=False)
+            content_html = rpy.get("content", "")
+            if not content_html:
+                raise ClickException("No content found")
+
         content_html = content_html.replace(
             'href="about:blank/', 'href="../'
         )  # Fix for readability replacing ".." with "about:blank"
