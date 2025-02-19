af353108a67a6481c07537dbea94f49628f883fa
Vlad Iliescu <vladiliescu@users.noreply.github.com>
2025-02-19 18:18:26 +0200

    fix: don't request gzipped content and not do anything with it.

--- grabit.py
+++ grabit.py
@@ -454,7 +454,6 @@ def download_html_content(url, user_agent: str) -> str:
         request_headers = {
             "User-Agent": user_agent,
             "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
         }
 
         response = requests.get(url, headers=request_headers)
